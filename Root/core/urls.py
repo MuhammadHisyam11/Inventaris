08e773fe6fcc1f12f61ebf084f1acdceb1de69fd
MuhammadHisyam11 <muhammad11hisyam@gmail.com>
2026-01-22 00:43:26 +0700

    Refactor templates and views for user management and inventory features
    - Updated footer and header templates to reflect new branding and navigation.
    - Simplified header navigation by removing unused elements and adding dynamic user information.
    - Implemented login functionality with session management in views.
    - Added logout functionality to clear user sessions.
    - Created custom login required decorator for protected views.
    - Developed inventory and user management templates with structured tables.
    - Introduced a 404 error page for better user experience on invalid routes.
    - Updated URL patterns to include new views for dashboard, management user, and inventory.

--- Root/core/urls.py
+++ Root/core/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('', views.base, name='base'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout, name='logout'),
     path('forgot-password/', views.forget, name='forgot-password'),
     path("reset/<uidb64>/<token>/", views.reset_password_confirm, name="password_reset_confirm"),
     path('register/', views.register, name='register'),
+    
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('management-user/', views.manajement_user, name='management-user'),
+    path('inventory/', views.inventory, name='inventory'),
 ]
